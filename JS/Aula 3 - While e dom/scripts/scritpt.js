e0f02e78fd82473b45744cc603a08731c0b184c0
Joao <79120634+vitorbarb@users.noreply.github.com>
2024-11-19 17:39:20 -0300

    aula 3

---
+++ JS/Aula 3 - While e dom/scripts/scritpt.js
@@ -0,0 +1,28 @@
+// DOM 
+// URL - location
+// botao de voltar - history
+// SITE / CONTEUDO - document
+
+//
+// window.location = "https://en.wikipedia.org/wiki/Bj%C3%B6rk"                     
+
+document.getElementById("catalogo").innerHTML = '<h3>Oi</h3>'
+
+//document - documento
+// GetElementById - Busca elemento por id
+//inner - insere algo dentro da pagina
+
+
+document.querySelector("")
+
+
+function registrarNome(){
+     nome = document.querySelector('#inputNome').value 
+    
+     document.querySelector("#catalogo").innerHTML=  `
+     <div id="nomeDiv">
+    <p>${nome} and bjork, are the biggest artist alive </p>
+     </div>
+ `
+}
+
